9924ca742fc72fbb3d700e5cda824304ec2cbbee
Bayuxz <107552948+Bayuxz@users.noreply.github.com>
2022-07-03 06:24:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -56,8 +56,8 @@ fs.watchFile(file_exif, () => {
 })
 
 // Sticker WM
-global.packname = sticker_name
-global.author = sticker_author
+global.packname = XD
+global.author = syinxd
 global.wm = '©Syinxd'
 
 global.multiplier = 1000 // The higher, The harder levelup
